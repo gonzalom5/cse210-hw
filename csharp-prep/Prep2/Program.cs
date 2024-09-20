f6515c9d8c9f8661a5afbc4acd2bd139a571be04
Gonzalo Martinez <mar17004@byui.edu>
2024-09-20 14:46:51 -0300

    Completed Prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,44 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep2 World!");
+    {   
+
+        Console.Write("Please enter your grade percentage: ");
+        string value = Console.ReadLine();
+
+        int x = int.Parse(value);
+
+        if (x >= 90)
+        {
+            Console.WriteLine("Your grade is A");
+        }
+        else if (x >= 80)
+        {
+            Console.WriteLine("Your grade is B");
+        }
+        else if (x >= 70)
+        {
+            Console.WriteLine("Your grade is C");
+        }
+        else if (x >= 60)
+        {
+            Console.WriteLine("Your grade is D");
+        }
+        else
+        {
+            Console.WriteLine("Your grade is F");
+        }
+
+          if (x >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time!");
+        }
     }
 }
