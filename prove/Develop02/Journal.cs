28aa964718d595fd10e8ba136e10151d9961266b
Gonzalo Martinez <mar17004@byui.edu>
2024-10-12 18:16:21 -0300

    Completed Journal Assignment

---
+++ prove/Develop02/Journal.cs
@@ -0,0 +1,55 @@
+using System;
+using System.IO;
+public class Journal
+{
+    public List<Entry> _entries = new List<Entry>();
+
+    public void AddEntry(Entry NewEntry)
+    {
+        _entries.Add(NewEntry);
+    }
+
+    public void DisplayAll()
+    {
+        foreach (Entry entry in _entries)
+        {
+            entry.Display();
+        }
+    }
+
+    public void LoadFromFile()
+    {
+        Console.WriteLine("What is the filename to load?");
+        string fileName = Console.ReadLine();
+        string[] lines = System.IO.File.ReadAllLines(fileName);
+
+        foreach (string line in lines)
+        {
+            string[] parts = line.Split(",");
+            
+            Entry entry = new Entry
+            {
+                _date = parts[0],
+                _promptText = parts[1],
+                _entryText = parts[2]
+            };
+            _entries.Add(entry);
+        }
+        Console.WriteLine("Journal loaded successfully!\n");
+    }
+
+    public void SaveToFile()
+    {
+        Console.WriteLine("What is the filename to save?");
+        string fileName = Console.ReadLine(); 
+
+        using (StreamWriter outputFile = new StreamWriter(fileName))
+        {
+            foreach (Entry entry in _entries)
+            {
+                outputFile.WriteLine($"{entry._date},{entry._promptText},{entry._entryText}");
+            }
+        }
+        Console.WriteLine("Journal saved successfully!\n");
+    }
+}
--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,9 +1,57 @@
 using System;
-
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        PromptGenerator promptGenerator = new PromptGenerator();
+        Journal journal = new Journal();
+        Console.WriteLine("Welcome to the Journal Program!");
+
+        bool isTrue = true;
+        while (isTrue)
+        {
+            Console.WriteLine("Please select one of the following choices:");
+            Console.WriteLine("1. Write");
+            Console.WriteLine("2. Display");
+            Console.WriteLine("3. Load");
+            Console.WriteLine("4. Save");
+            Console.WriteLine("5. Quit");
+            Console.Write("What would you like to do? ");
+
+            string userInput = Console.ReadLine();
+
+            if (userInput == "1")
+            {
+                string randomPrompt = promptGenerator.GetRandomPrompt();
+                Console.WriteLine(randomPrompt);
+
+                string userResponse = Console.ReadLine();
+
+                Entry entry = new Entry()
+                {
+                    _date = DateTime.Now.ToShortDateString(),
+                    _promptText = randomPrompt,
+                    _entryText = userResponse
+                };
+
+                journal.AddEntry(entry);
+            }
+            else if (userInput == "2")
+            {
+                journal.DisplayAll();
+            }
+            else if (userInput == "3")
+            {
+                journal.LoadFromFile();
+            }
+            else if (userInput == "4")
+            {
+                journal.SaveToFile();
+            }
+            else if (userInput == "5")
+            {
+                isTrue = false;
+            }
+        }
     }
 }
