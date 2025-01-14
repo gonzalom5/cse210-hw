d0ea64408585379d06450b1f1598b69e3b6b1dee
Gonzalo Martinez <mar17004@byui.edu>
2025-01-14 17:55:19 -0300

    completed Prep1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -6,7 +6,7 @@ class Program
     {
         Console.Write("What is your first name? ");
         string fname = Console.ReadLine();
-        Console.WriteLine("What is your last name? ");
+        Console.Write("What is your last name? ");
         string lname = Console.ReadLine();
         Console.WriteLine($"Your name is {lname}, {fname} {lname}.");
     }
