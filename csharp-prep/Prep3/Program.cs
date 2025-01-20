8413aea5ea280538988f39fa71e600098f91b4c1
Gonzalo Martinez <mar17004@byui.edu>
2025-01-20 11:32:38 -0300

    Completed Prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,32 @@
 using System;
+using System.Net;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int num = randomGenerator.Next(1, 101);
+
+        int guess = -1;
+        
+        while (num != guess)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (num > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (num < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
     }
 }
